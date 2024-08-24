909419c8a97342d2796488a92f6c939e5b91aa5a
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2024-08-24 09:37:58 +0000

    start project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-teal-700">
+   MinuttMind-API Project start
+  </h1>
+
+  )
+}
+
+export default App
